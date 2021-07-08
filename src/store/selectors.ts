0cc14522f5de82d9c0ab9950df22f056823e2457
artem <70851610+artemdov@users.noreply.github.com>
2021-07-08 12:53:00 +0300

    refactoring

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -4,4 +4,4 @@ export const isLoggedInSelector = (state: AppRootStateType) =>
   state.auth.isLoggedIn;
 export const isLoadingSelector = (state: AppRootStateType) =>
   state.auth.loading;
-export const setUserSelector = (state: AppRootStateType) => state.auth.user;
+export const getUserSelector = (state: AppRootStateType) => state.auth.user;
