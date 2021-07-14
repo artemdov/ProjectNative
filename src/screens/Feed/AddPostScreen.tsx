ee16bb2f925282ec9a87f6bcae680647e79dd674
artem <70851610+artemdov@users.noreply.github.com>
2021-07-14 22:20:41 +0300

    add image crop picker

--- src/screens/Feed/AddPostScreen.tsx
+++ src/screens/Feed/AddPostScreen.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import {StyleSheet, View} from "react-native";
+import {StyleSheet} from "react-native";
 import {InputField, InputWrapper, PostContainer} from "../../styles/AddPostStyles";
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
+import ImagePicker from 'react-native-image-crop-picker';
 
 export const AddPostScreen = () => {
+
+    const takePhotoFromCamera = () => {
+        ImagePicker.openCamera({
+            width: 300,
+            height: 400,
+            cropping: true
+        }).then(image => {
+            console.log(image);
+        });
+    }
+    const choosePhotoFromLibrary = () => {
+        ImagePicker.openPicker({
+            width: 300,
+            height: 400,
+            cropping: true
+        }).then(image => {
+            console.log(image);
+        });
+    }
+
     return (
         <PostContainer>
             <InputWrapper>
@@ -14,11 +35,10 @@ export const AddPostScreen = () => {
                 </InputField>
             </InputWrapper>
             <ActionButton buttonColor="rgba(231,76,60,1)">
-                <ActionButton.Item buttonColor='#9b59b6' title="Сделать фото" onPress={() => console.log("notes tapped!")}>
+                <ActionButton.Item buttonColor='#9b59b6' title="Сделать фото" onPress={takePhotoFromCamera}>
                     <Icon name="camera-outline" style={styles.actionButtonIcon}/>
                 </ActionButton.Item>
-                <ActionButton.Item buttonColor='#3498db' title="Выбрать из галереи" onPress={() => {
-                }}>
+                <ActionButton.Item buttonColor='#3498db' title="Выбрать из галереи" onPress={choosePhotoFromLibrary}>
                     <Icon name="images-outline" style={styles.actionButtonIcon}/>
                 </ActionButton.Item>
             </ActionButton>
